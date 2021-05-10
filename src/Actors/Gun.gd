55ab3863e494a3fe4074d1cebccb883ef2622916
Jaakko <lintulajaakko@gmail.com>
2021-05-10 14:58:53 +0300

    fixed bugs

--- src/Actors/Gun.gd
+++ src/Actors/Gun.gd
@@ -3,23 +3,32 @@ class_name Gun
 
 export var bullet_speed = 0
 export var fire_rate = 0.0
+
+#var bullet
+var ammo_type
+var can_fire = true
 var arrows = preload("res://src/Actors/Bullets/BowArrow.tscn")
 var rifle_rounds = preload("res://src/Actors/Bullets/RifleRound.tscn")
-#var ammo_type
-var bullet
-var can_fire = true
 
-var current_weapon
+
 onready var wp_1 = $Rifle
 onready var wp_2 = $Bow
+var current_weapon = wp_1
 
 func _unhandled_input(event: InputEvent) -> void:
 	look_at(get_global_mouse_position())
 	#var call = ray.get_collider()
 
+	if Input.is_action_pressed("weapon_1"):
+		current_weapon = wp_1
+		ammo_type = rifle_rounds
+	if Input.is_action_pressed("weapon_2"):
+		current_weapon = wp_2
+		ammo_type = arrows
+	
 #-------------------------------------------------------------------------------
 	if event.is_action_pressed("shoot") and can_fire:
-		var bullet_instance = bullet.instance()
+		var bullet_instance = ammo_type.instance()
 		bullet_instance.position = global_position
 		bullet_instance.rotation_degrees = rotation_degrees - 90
 		bullet_instance.apply_impulse(Vector2(), Vector2(bullet_speed, 0).rotated(rotation))
@@ -27,4 +36,6 @@ func _unhandled_input(event: InputEvent) -> void:
 		can_fire = false
 		yield(get_tree().create_timer(fire_rate), "timeout")
 		can_fire = true
+	
+	
 
--- src/Actors/Guns/Bow.gd
+++ src/Actors/Guns/Bow.gd
@@ -4,4 +4,4 @@ extends Gun
 func _ready() -> void:
 	bullet_speed = 2500
 	fire_rate = 0.01
-	bullet = preload("res://src/Actors/Bullets/BowArrow.tscn")
+	#bullet = preload("res://src/Actors/Bullets/BowArrow.tscn")
