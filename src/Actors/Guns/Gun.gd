0b054d007a8d3aaf1a3d09fbf143c4f567ca2d7a
Jaakko <lintulajaakko@gmail.com>
2021-05-17 16:39:01 +0300

    hand mechanics

--- src/Actors/Guns/Gun.gd
+++ src/Actors/Guns/Gun.gd
@@ -30,23 +30,10 @@ func _input(event: InputEvent) -> void:
 		ammo_type = arrows
 		can_fire = true
 
-	
-
-
-
 #-------------------------------------------------------------------------------
 	if event.is_action_pressed("shoot") and can_fire:
 		shoot()
 
-#		var bullet_instance = ammo_type.instance()
-#		bullet_instance.position = global_position
-#		bullet_instance.rotation_degrees = rotation_degrees - 90
-#		bullet_instance.apply_impulse(Vector2(), Vector2(bullet_speed, 0).rotated(rotation))
-#		get_tree().get_root().add_child(bullet_instance)
-#
-#		can_fire = false
-#		yield(get_tree().create_timer(fire_rate), "timeout")
-#		can_fire = true
 
 func shoot():
 	look_at(get_global_mouse_position())
