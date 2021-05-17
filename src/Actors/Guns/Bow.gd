6df6f8a521595afd0848d4cc437120addc80f450
lintulajaakko <lintulajaakko@gmail.com>
2021-05-17 10:28:15 +0300

    bow animation

--- src/Actors/Guns/Bow.gd
+++ src/Actors/Guns/Bow.gd
@@ -1,5 +1,6 @@
 extends Gun
 
+onready var animation = $AnimationPlayer
 
 func _ready() -> void:
 	bullet_speed = 2500
@@ -8,7 +9,10 @@ func _ready() -> void:
 	gun_id = 2
 	damage = 5
 
-
+ 
+func animation():
+	if Input.is_action_pressed("shoot"):
+		animation.play("Shoot_left")
 
 func _on_Player_weapon_switched(type: int) -> void:
 	if type != gun_id:
